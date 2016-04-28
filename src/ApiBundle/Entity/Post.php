443dc6413b605b6d3f0f3c6ae2037c7b9be849e6
Vlady <vladronaldo7@gmail.com>
2016-04-28 13:57:29 +0300

    added entity post

---
+++ src/ApiBundle/Entity/Post.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ubuntu
+ * Date: 4/28/16
+ * Time: 1:33 PM
+ */
+
+namespace ApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="posts")
+ *
+ */
+class Post implements \JsonSerializable
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    /**
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    protected $title;
+
+    /**
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    protected $description;
+
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            'id' => $this->id,
+            'title'=> $this->title,
+            'description'=> $this->description,
+        ];
+    }
+}
