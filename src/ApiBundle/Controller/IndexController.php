c9e2a7ec5bcf7d0effc44136e1802d3b983dc34e
Vlady <vladronaldo7@gmail.com>
2016-04-28 14:32:04 +0300

    put some staff in IndexController

--- src/ApiBundle/Controller/IndexController.php
+++ src/ApiBundle/Controller/IndexController.php
@@ -10,8 +10,25 @@ namespace ApiBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use ApiBundle\Entity\Post;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class IndexController extends Controller
 {
+    public function newAction(Request $request)
+    {
+        $post = new Post();
+
+        $response = new JsonResponse();
+
+        $form = $this->createFormBuilder($post)
+            ->add('title', TextType::class)
+            ->add('description', TextType::class)
+            ->getForm();
+
+        return $response;
+    }
 
 }
