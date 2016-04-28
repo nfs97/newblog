e32d736886413f35a0be5009e3f3e5ea8996388c
Vlady <vladronaldo7@gmail.com>
2016-04-28 13:32:28 +0300

    added ApiBundle and stuff that it depends on

---
+++ src/ApiBundle/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ubuntu
+ * Date: 4/28/16
+ * Time: 1:29 PM
+ */
+
+namespace ApiBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class IndexController extends Controller
+{
+
+}
